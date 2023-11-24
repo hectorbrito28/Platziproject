8f8d9d75610b5317ae6c1a6321bc92942d365aad
SIMON <hectorsibritoa@gmail.com>
2023-11-24 15:13:44 -0400

    Try firebase implements

--- Platzi/settings.py
+++ Platzi/settings.py
@@ -189,7 +189,7 @@ if not DEBUG:
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ###Static
@@ -202,7 +202,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 
 }
